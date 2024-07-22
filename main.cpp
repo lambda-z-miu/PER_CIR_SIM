120ffac7b2f64c82a2c669dc131643bfc3ca90cb
lambda-z-miu <2300012806@stu.pku.edu.cn>
2024-07-22 23:00:12 +0800

    eigen

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <algorithm>
 #include <deque>
 #include <windows.h>
-#include "linear.h"
 #include "graph.h"
 #include "devices.h"
+
+#include <eigen-3.4.0/Eigen/Core>
+#include <eigen-3.4.0/Eigen/Dense>
 
 
 using namespace std;
@@ -18,36 +20,43 @@ int main(){
 //  graph.print();
     auto p=graph.findloop();
 
-/*
-    for(auto i : p){
-        for(auto j : i){
-            std::cout<<j.first<<"->"<<j.second<<" ";
-        }
-        std::cout<<std::endl;
-    }
-*/    
-
     auto test=graph.getequation(p);
-/*
-    for(auto i : test){
-        for(auto j : i){
-            std::cout<<setw(12)<<j<<" ";
-        }
-        std::cout<<std::endl;
-    }
-    std::cout<<std::endl;
-*/
+
 
     Matrix indexs{test};
-    auto answers=indexs.solve();
+
+
+    int rows=test.size();
+    Eigen::MatrixXcd L(rows,rows);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < rows; ++j) {
+            L(i, j) = test[i][j];
+        }
+    }
+    //std::cout<<L<<std::endl;
+
+
+    Eigen::VectorXcd R(rows);
+    for(int i=0;i<rows;i++){
+        R(i)=test[i].back();
+    }
+    //std::cout<<R;
+
+    Eigen::JacobiSVD<Eigen::MatrixXcd> svd(L, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::VectorXcd answer = svd.solve(R);
 
     auto time_1=clock();
-
     std::cout<<"After "<<(time_1-time_0)/1000.01<<" seconds :"<<std::endl;
-    
-    for(int i=0;i<answers.size();i++) {
+
+    //std::cout<<std::endl<<"!!"<<std::endl<<answer<<std::endl;
+
+    for(int i=0;i<answer.size();i++) {
         std::cout<<graph.edges[i].first<<"->"<<graph.edges[i].second<<": i="<<
-        answers[i].real()<<(answers[i].imag()<0 ? "" : "+")<<answers[i].imag()<<"j";
+        answer[i].real()<<(answer[i].imag()<0 ? "" : "+")<<answer[i].imag()<<"j";
         std::cout<<std::endl;
     }
+
+
+    //auto answers=indexs.solve();
+
 }
